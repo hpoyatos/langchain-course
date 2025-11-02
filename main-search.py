aad11448d5a90a3f41acbdae1543905b98796ed9
Henrique Poyatos <henrique.poyatos@ulife.com.br>
2025-11-02 13:27:29 -0300

    Exemplo de Agente de Busca com Tavily

---
+++ main-search.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model_name="gpt-4")#, temperature=0)
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,   
+    prompt=react_prompt,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+
+
+def main():
+    result = chain.invoke(
+        {
+            "input": "Procure no Linkedin por 3 vagas de emprego recentes que mencionam conhecimento em Langchain como habilidade do candidato. Liste seus detalhes"
+        }
+    )
+    print(result)
+
+if __name__ == "__main__":
+    main()
