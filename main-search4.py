84e3a837da21d6700cdb8884793eb523bcd9a50a
Henrique Poyatos <henrique.poyatos@ulife.com.br>
2025-11-02 14:32:42 -0300

    Saída estruturada

---
+++ main-search4.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama 
+from langchain_tavily import TavilySearch
+
+#Pydantic
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model_name="gpt-4")  # , temperature=0)
+structured_llm = llm.with_structured_output(AgentResponse)
+
+react_prompt = hub.pull("hwchase17/react")
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions="")
+
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+
+chain = agent_executor | extract_output | structured_llm
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+
+
+def main():
+    result = chain.invoke(
+        {
+            "input": "Procure no Linkedin por 3 vagas de emprego recentes em São Paulo/SP que mencionam conhecimento em Langchain como habilidade do candidato. Liste seus detalhes"
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
